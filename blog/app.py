65844966e531910937449e892dbf7912266b4959
natalyabystrova <98619051+natalyabystrova@users.noreply.github.com>
2023-02-27 15:11:57 +0300

    lesson_3 (#3)
    Co-authored-by: Наталья <Tash@MacBook-Air-Natala.local>

--- blog/app.py
+++ blog/app.py
@@ -3,6 +3,8 @@ from time import time
 from werkzeug.exceptions import BadRequest
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.models.database import db
+from blog.views.auth import login_manager, auth_app
 
 app = Flask(__name__)
 
@@ -67,6 +69,7 @@ def power_value():
     app.logger.debug("%s ** %s = %s", x, y, result)
     return str(result)
 
+
 @app.route("/divide-by-zero/")
 def do_zero_division():
     return 1 / 0
@@ -81,3 +84,36 @@ def handle_zero_division_error(error):
 app.register_blueprint(users_app, url_prefix="/users")
 
 app.register_blueprint(articles_app, url_prefix="/articles")
+
+app.config["SECRET_KEY"] = "abcdefg123456"
+app.register_blueprint(auth_app, url_prefix="/auth")
+login_manager.init_app(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal: flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'> """
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+
+    print("done! created users:", admin, james)
