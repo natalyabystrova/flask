f8e62597e7b4ecb2e1bea99e173d35fc1497233a
natalyabystrova <98619051+natalyabystrova@users.noreply.github.com>
2023-03-08 13:57:21 +0300

    lesson_5 (#5)
    Co-authored-by: Наталья <Tash@MacBook-Air-Natala.local>

--- blog/configs.py
+++ blog/configs.py
@@ -1,15 +1,19 @@
 import os
 
+
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = "postgresql://user:password@localhost:5432/blog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "abcdefg123456"
+    WTF_CSRF_ENABLED = True
+
 
 class DevConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
 
 class TestingConfig(BaseConfig):
     TESTING = True
